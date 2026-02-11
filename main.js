867e6a257478de7a6304ff9ad8d2c1556da0f697
gialli24 <a.gialli2409@gmail.com>
2026-02-11 15:03:28 +0100

    Add option 2 with external function call using parameters

--- main.js
+++ main.js
@@ -5,15 +5,34 @@ const bulb = document.getElementById("bulb_image");
 console.log(bulb, button);
 
 /* Listener with anonymus function */
-button.addEventListener("click", function() {
+/* button.addEventListener("click", function() {
     const buttonLabel = button.innerHTML;
-    
-    /* Check if turn on or off */
     if (buttonLabel.includes("Accendi")) {
         bulb.src = "./img/yellow_lamp.png";
         button.innerHTML = "Spegni";
     } else {
         bulb.src = "./img/white_lamp.png";
         button.innerHTML = "Accendi";
+    }
+}) */
+
+function switchBulb(action) {
+    if (action === "turn_on") {
+        bulb.src = "./img/yellow_lamp.png";
+        button.innerHTML = "Spegni";
+    } else {
+        bulb.src = "./img/white_lamp.png";
+        button.innerHTML = "Accendi";
+    }
+}
+
+button.addEventListener("click", function() {
+    
+    const buttonLabel = button.innerHTML;
+
+    if (buttonLabel.includes("Accendi")) {
+        switchBulb("turn_on"); 
+    } else {
+        switchBulb("turn_off"); 
     }
 })
